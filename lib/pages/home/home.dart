1388045851d2cfbe5dbe8e67ace109529c2dee1c
siddharth <smishra1605@gmail.com>
2020-05-27 21:27:17 +0530

    Home Page Done

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:whattsappclone/pages/home/status_screen.dart';
+
+import 'calls_screen.dart';
+import 'camera_page.dart';
+import 'chats_screen.dart';
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("WhatsApp",style: TextStyle(fontWeight: FontWeight.bold),),
+          actions: <Widget>[
+            IconButton(icon:Icon(Icons.search,color: Colors.white,),onPressed: null,),
+            IconButton(icon: Icon(Icons.more_vert,color: Colors.white,), onPressed: null)
+          ],
+          bottom: TabBar(
+            indicatorColor: Color(0xFF075E54),
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text:"CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            )
+          ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CameraPage(),
+          ChatsScreen(),
+          StatusScreen(),
+          CallsScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/home/status_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class StatusScreen extends StatefulWidget {
+  @override
+  _StatusScreenState createState() => _StatusScreenState();
+}
+
+class _StatusScreenState extends State<StatusScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          "ASD"
+      ),
+    );
+  }
+}
