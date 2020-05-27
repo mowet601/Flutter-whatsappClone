fda6e309694cd22844c336e6ef790d981986a3f5
siddharth <smishra1605@gmail.com>
2020-05-27 20:02:11 +0530

    Splash  timer  Added

--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -1,10 +1,18 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:whattsappclone/pages/home/home.dart';
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2),()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home())));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
