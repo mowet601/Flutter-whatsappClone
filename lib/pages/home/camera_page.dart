c332de91e47c74a3389f6b32b3d5e2f9e0f72e78
siddharth <smishra1605@gmail.com>
2020-05-29 11:16:03 +0530

    APK added

--- lib/pages/home/camera_page.dart
+++ lib/pages/home/camera_page.dart
@@ -3,7 +3,6 @@ class CameraPage extends StatefulWidget {
   @override
   _CameraPageState createState() => _CameraPageState();
 }
-
 class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
